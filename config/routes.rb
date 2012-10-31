675e17619ea8d7fc14305482848da4e1ab12665c
Khanh <nguyenvietnamkhanh@gmail.com>
2012-10-31 11:00:56 +0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 GetPrivacyContent::Application.routes.draw do
-  get "policies/privacy"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +49,8 @@ GetPrivacyContent::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  match 'privacy' => 'policies#privacy', :as => :privacy
+  root :to => 'policies#privacy'
 
   # See how all your routes lay out with "rake routes"
 
